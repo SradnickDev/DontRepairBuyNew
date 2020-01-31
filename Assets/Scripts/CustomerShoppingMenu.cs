4cd9a65c9e8164497ce0411a8020fa8ce894c4bb
Oliver <info.sradnick@gmx.de>
2020-01-31 21:57:26 +0100

    init commit

---
+++ Assets/Scripts/CustomerShoppingMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NaughtyAttributes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ProductButton : MonoBehaviour
+{
+	[SerializeField] private Button m_button;
+	[SerializeField] private Image m_countDown;
+	[SerializeField] private TextMeshProUGUI m_countDownText;
+
+	private void Awake()
+	{
+		m_button = GetComponent<Button>();
+	}
+	
+	//public void Setup(Sprite icon)
+}
+
+public class CustomerShoppingMenu : MonoBehaviour
+{
+	[SerializeField, ReorderableList]
+	private List<Product> m_products = new List<Product>();
+
+	[SerializeField] private ProductButton m_productButton;
+}
---
+++ Assets/Scripts/Product.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Product : MonoBehaviour
+{
+	public string Name;
+	public int Points;
+	public Sprite Icon;
+	private BoxCollider m_collider;
+
+	private void Awake()
+	{
+		m_collider = GetComponent<BoxCollider>();
+	}
+
+	private void Start()
+	{
+		
+	}
+}
