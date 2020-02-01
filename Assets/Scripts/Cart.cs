169f27d3e4df9733badbc9303efb7ad86264d792
Oliver <info.sradnick@gmx.de>
2020-02-01 22:55:39 +0100

    products
    products have to be scanned before they count as bought

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -1,27 +1,27 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(BoxCollider))]
 [RequireComponent(typeof(AudioSource))]
 public class Cart : MonoBehaviour
 {
-    [SerializeField] private CustomerShoppingMenu customerShopping;
-    [SerializeField] private AudioClip m_clip;
+	[SerializeField] private CustomerShoppingMenu customerShopping;
+	[SerializeField] private AudioClip m_clip;
 	[SerializeField] private AudioSource m_audioSource;
 	[SerializeField] private ScoreData m_score;
 	private Dictionary<Product, int> m_shoppingList;
-
 
 	private void OnTriggerEnter(Collider other)
 	{
 		var product = other.gameObject.GetComponent<Product>();
 		if (product == null) return;
-        if (!product.enabled) return;
+		if (!product.Bought) return;
+		if (!product.enabled) return;
 
-        customerShopping.RemoveProductFromShoppingList(product);
+		customerShopping.RemoveProductFromShoppingList(product);
 
 		m_audioSource.PlayOneShot(m_clip);
 		m_score.AddScore(1);
-        product.enabled = false;
-
-    }
+		product.enabled = false;
+	}
 }
