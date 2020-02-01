bc31c2fe70ccffb580ee98d0c58abb10f8cfb99c
Oliver <info.sradnick@gmx.de>
2020-02-01 16:51:19 +0100

    desk feedback

---
+++ Assets/Scripts/Cashier.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Cashier : MonoBehaviour
+{
+	[SerializeField] private ScoreData m_scoreData;
+	[SerializeField] private TextMeshPro m_scoreText;
+
+	private void Update()
+	{
+		m_scoreText.text =  "money : "+ m_scoreData.Amount;
+	}
+}
