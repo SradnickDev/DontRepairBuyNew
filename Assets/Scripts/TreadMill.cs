80ba91991b462ae451029f07333715c5a2377a9a
Pkerth4hop <Bryan.t@web.de>
2020-02-01 21:20:07 +0100

    Treadmill should scroll but it doesnt :c

--- Assets/Scripts/TreadMill.cs
+++ Assets/Scripts/TreadMill.cs
@@ -1,10 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class TreadMill : MonoBehaviour
 {
+    [SerializeField] private float m_scrollSpeed = 0.5f;
+    
     [SerializeField] private Vector3 m_dir;
     [SerializeField] private float m_pullForce;
     [SerializeField] private ForceMode m_forceMode;
+    [SerializeField] private MeshRenderer renderer;
+
+
+    private void Update()
+    {
+        float offset = Time.time * m_scrollSpeed;
+        
+        var material = renderer.material;
+        
+        var posX = material.mainTextureOffset.x;
+        var posY = material.mainTextureOffset.y;
+        
+        var newPos = new Vector2(offset ,posY);
+
+        material.mainTextureOffset = newPos;
+    }
 
     private void OnTriggerStay(Collider other)
     {
