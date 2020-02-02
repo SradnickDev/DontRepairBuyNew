8a42d3283d0ad899ce4ccd2ba7c2fbdb121bc0ff
Oliver <info.sradnick@gmx.de>
2020-02-02 10:14:51 +0100

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void LoadGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void Exit()
+	{
+	#if UNITY_EDITOR
+		if (EditorApplication.isPlaying)
+		{
+			EditorApplication.isPlaying = false;
+		}
+	#else
+		Application.Quit();
+	#endif
+	}
+}
