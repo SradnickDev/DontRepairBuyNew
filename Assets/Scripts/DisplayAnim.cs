4ccc2a884f923d1a776f4ea15744c4cc8ada302a
Oliver <info.sradnick@gmx.de>
2020-02-02 13:21:14 +0100

    new anim

--- Assets/Scripts/DisplayAnim.cs
+++ Assets/Scripts/DisplayAnim.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class DisplayAnim : MonoBehaviour
 {
+    public Image m_crack;
+    public Sprite m_crack2;
+    public Sprite m_crack3;
     public UnityEvent OnBrokenDisplay;
 
     public void InvokeOnBrokenDisplay()
     {
         OnBrokenDisplay?.Invoke();
+    }
+
+    public void UseCrack2()
+    {
+        m_crack.sprite = m_crack2;
+    }
+    
+    public void UseCrack3()
+    {
+        m_crack.sprite = m_crack3;
     }
 }
