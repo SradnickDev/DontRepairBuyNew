b7834087f09dc14a0659c869f494e146fb3aa4e1
lucas-martinic <lucas.martinic@gmail.com>
2020-02-02 11:29:53 +0100

    debug btn

---
+++ Assets/EnableDisable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    bool enabled = true;
+    public GameObject menu;
+    
+    public void EnDis()
+    {
+        if (enabled)
+        {
+            menu.SetActive(false);
+            enabled = false;
+        }
+        else
+        {
+            menu.SetActive(true);
+            enabled = true;
+        }
+    }
+}
